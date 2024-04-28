6ba21dcd4c3f99f8e3e5af2a3ad09efc7cc7b2de
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2024-04-28 17:48:43 +0200

    change db config

--- src/db/db.php
+++ src/db/db.php
@@ -2,7 +2,7 @@
 
 require_once "./vendor/autoload.php";
 
-use Doctrine\DBAL\DriverManager;
+/* use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
 
@@ -21,4 +21,19 @@ $connection = DriverManager::getConnection([
     "driver" => "pdo_pgsql",
 ], $config);
 
-$entityManager = new EntityManager($connection, $config);
+$entityManager = new EntityManager($connection, $config); */
+
+
+$host = "aws-0-eu-central-1.pooler.supabase.com";
+$name = "postgres";
+$port = 5432;
+$user = "postgres.wzqbaxbadiqwdodpcglt";
+$password = "J%3!$qo6x#!S8o5Y@iJVTmt8BJ4G@TU97RyirUW29bamcUg@JDYrhod3$fBzXH6sKoQ8g3YRzmo&pnfR6bzzFD9yGJgxG8yZfq6";
+
+$dsn = "pgsql:host=$host;dbname=$name;port=$port";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$db = new PDO($dsn, $user, $password, $opt);
