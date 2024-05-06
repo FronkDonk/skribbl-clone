58c3c26cb418cb34c79bd41f302449bf6dbc726d
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2024-05-06 12:53:02 +0200

    yup

--- src/views/auth/sign-in.php
+++ src/views/auth/sign-in.php
@@ -11,8 +11,8 @@
 <body>
     <?php
     require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
-
-
+    //yup
+    
     use Jenssegers\Blade\Blade;
 
     $blade = new Blade($_SERVER['DOCUMENT_ROOT'] . '/src/components/ui', $_SERVER['DOCUMENT_ROOT'] . '/src/components/ui/cache');
