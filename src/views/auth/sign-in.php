f01637a07eb77890126c2717405e23a9cbd4f65d
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2024-05-06 12:49:36 +0200

    lite småfixar

--- src/views/auth/sign-in.php
+++ src/views/auth/sign-in.php
@@ -79,7 +79,8 @@
             </div>
         </div>
         <div class="hidden bg-muted lg:block">
-            <img src="/placeholder.svg" alt="Image" width="1920" height="1080" class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
+            <img src="/placeholder.svg" alt="Image" width="1920" height="1080"
+                class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
         </div>
     </div>
     <script src="/dist/signIn.bundle.js"></script>
